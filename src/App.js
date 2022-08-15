cccd9d4cea44714bad3ed9128c208e863a72b941
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-14 23:21:34 -0400

    about me and contact form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
+    const [contactSelected, setContactSelected] = useState(false);
+    
+    
     return (
         <div>
-            <Nav />
+            <Nav
+            contactSelected={contactSelected}
+            setContactSelected={setContactSelected}
+            ></Nav>
             <main>
-                <About />
+                {!contactSelected ? (
+                    <>
+                    <About></About>
+                    </>
+                  ) : (
+                    <ContactForm></ContactForm>
+                )}
             </main>
         </div>
     );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-
-    return (
+function Nav(props){
+    const{
+        contactSelected,
+        setContactSelected
+    } = props;
+    return(
         <header data-testid="header" className="flex-row px-1">
             <h2>
                 <a href="/">
-                    Christian Carvajal
+                    <span>Christian Carvajal</span>
                 </a>
             </h2>
             <nav>
                 <ul className='flex-row'>
                     <li className='mx-2'>
-                        <a href="#about" >About Me</a>
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>About Me</a>
                     </li>
-                    <li className='mx-2'>
-                        Portfolio
-                    </li>
-                    <li className='mx-2'>
-                        Contact
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                 </ul>
             </nav>
         </header>
-    );
+    )
 }
 
 export default Nav;
