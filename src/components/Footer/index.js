2e6bd92824c2a531cc722ab493a25361fcb804d8
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-17 20:59:39 -0400

    links added to portfolio and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,9 +8,15 @@ function Footer() {
     return(
         <footer>
             <ul className='flex-row space-between center'>
+                <a href="https://github.com/chriscarv" target="_blank" rel="noreferrer">
                 <img className="f-img-thumbnail" src={github} alt="github logo"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/christian-carvajal-48604018b/" target="_blank" rel="noreferrer">
                 <img className='f-img-thumbnail' src={linkdin} alt="linkdin logo"></img>
+                </a>
+                <a href="https://stackoverflow.com/users/18301641/christian-carvajal" target="_blank" rel="noreferrer">
                 <img className='f-img-thumbnail' src={stackover} alt="stackover flow logo"></img>
+                </a>
             </ul>
         </footer>
     )
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,8 +6,14 @@ function Portfolio() {
     return(
         <section >
             <h1>Portfolio</h1>
+             <h2>Spotify Now</h2>   
+            <a href="https://spotify-wrapped-now.herokuapp.com/" target="_blank" rel="noreferrer">
             <img src={spotifyNow} alt="spotify now" className='img-thumbnail mx-1'/>
+            </a>
+            <h2>Creative Dishes</h2>
+            <a href="https://chriscarv.github.io/dcl/" target="_blank"  rel="noreferrer">
             <img src={creativeDishes} alt="creative dishes" className='img-thumbnail mx-1'/>
+            </a>
         </section>
     )
 }
