e84dd6d96a1959ab75302ad5bff4b9c2fac5e19f
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-31 21:05:30 -0400

    add ticket app

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,6 +10,10 @@ function Portfolio() {
             <a href="https://spotify-wrapped-now.herokuapp.com/" target="_blank" rel="noreferrer">
             <img src={spotifyNow} alt="spotify now" className='img-thumbnail mx-1'/>
             </a>
+            <h2>TicketLess</h2>
+            <a href="https://desolate-savannah-98281.herokuapp.com/" target="_blank" rel="noreferrer">
+            <img src={require('../../assets/images/ticketless.png')} alt="ticket app" className='img-thumbnail mx-1' />
+            </a>
             <h2>Creative Dishes</h2>
             <a href="https://chriscarv.github.io/dcl/" target="_blank"  rel="noreferrer">
             <img src={creativeDishes} alt="creative dishes" className='img-thumbnail mx-1'/>
